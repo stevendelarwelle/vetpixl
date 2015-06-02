f7da9cf4164f7c6dcb5e375991ecb8e925670525
stevendelarwelle <stevendelarwelle@gmail.com>
2015-06-02 01:18:09 +0000

    chang to user name via facebook login

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,4 +9,13 @@ class RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
   end
+  
+  def update_resource(resource, params)
+    if current_user.provider == "facebook"
+      params.delete("current_password")
+      resource.update_without_password(params)
+    else
+      resource.update_with_password(params)
+    end
+  end
 end
