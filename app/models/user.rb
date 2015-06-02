307086ec2cd77fd8c5b73c349d2bb7ba6bc1e1b4
stevendelarwelle <stevendelarwelle@gmail.com>
2015-06-02 01:29:05 +0000

    added name to facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ActiveRecord::Base
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
+      user.name = auth.info.name
       user.password = Devise.friendly_token[0,20]
     end
   end
