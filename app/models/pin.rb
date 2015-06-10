6740e2a68d6fcebaf27b43ca5963dd06e7474dd9
stevendelarwelle <stevendelarwelle@gmail.com>
2015-06-10 01:25:20 +0000

    added jumbotron

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,7 @@
 class Pin < ActiveRecord::Base
-  acts_as_votable
+  
+	
+	acts_as_votable
   acts_as_taggable
   belongs_to :user
   has_attached_file :image, :styles => { :large => "500x500>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
